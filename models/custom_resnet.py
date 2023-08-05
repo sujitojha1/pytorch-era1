e17ecfaac7473afb4aea95c5c896c9833c635119
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-08-05 14:54:24 +0530

    minor changes

--- models/custom_resnet.py
+++ models/custom_resnet.py
@@ -1,5 +1,7 @@
 import torch.nn.functional as F
 import torch.nn as nn
+from pytorch_lightning import LightningModule
+from torchmetrics import accuracy
 
 dropout_value = 0.1
 
@@ -88,4 +90,57 @@ class Net(nn.Module):
         out = out.view(out.size(0),-1)
         out = self.fc(out)
 
-        return F.log_softmax(out, dim=-1)
+        # return F.log_softmax(out, dim=-1)
+        return out
+
+class LitCustomResnet(LightingModule):
+    def __init__(self, lr = 0.05):
+        super().__init__()
+        self.model = Net()
+        self.save_hyperparameters()
+
+    def forward(self,x):
+        return self.model(x)
+    
+    def training_step(self,batch,batch_id):
+        x,y = batch
+        logits = self(x)
+        loss = nn.CrossEntropyLoss(logits,y)
+        self.log("training loss", loss)
+        return loss
+    
+    def evaluate(self, batch, stage=None):
+        x,y = batch
+        logits = self(x)
+        loss = nn.CrossEntropyLoss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc= accuracy(preds,y)
+
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar=True)
+            self.log(f"{stage}_acc", acc, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "val")
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(
+            self.parameters(),
+            lr=self.hparams.lr,
+            momentum=0.9,
+            weight_decay=5e-4,
+        )
+        steps_per_epoch = 45000 // BATCH_SIZE
+        scheduler_dict = {
+            "scheduler": OneCycleLR(
+                optimizer,
+                0.1,
+                epochs=self.trainer.max_epochs,
+                steps_per_epoch=steps_per_epoch,
+            ),
+            "interval": "step",
+        }
+        return {"optimizer": optimizer, "lr_scheduler": scheduler_dict}
