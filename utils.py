1c1a38f0dde6061238e15bbea91143079604f573
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-08-06 10:46:09 +0530

    minor changes

--- utils.py
+++ utils.py
@@ -160,7 +160,49 @@ def plot_misclassified_images(model, test_loader, classes, device):
     plt.tight_layout()
     plt.show()
 
+def plot_grad_cam_images_custom_resnet(model, test_loader, classes, device):
+    # set model to evaluation mode
+    model.eval()
+    target_layers = [model.R3]
 
+    # Construct the CAM object once, and then re-use it on many images:
+    cam = GradCAM(model=model, target_layers=target_layers)
+
+    misclassified_images = []
+    actual_labels = []
+    actual_targets = []
+    predicted_labels = []
+    
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            _, pred = torch.max(output, 1)
+            for i in range(len(pred)):
+                if pred[i] != target[i]:
+                    actual_targets.append(target[i])
+                    misclassified_images.append(data[i])
+                    actual_labels.append(classes[target[i]])
+                    predicted_labels.append(classes[pred[i]])
+
+    # Plot the misclassified images
+    fig = plt.figure(figsize=(12, 5))
+    for i in range(10):
+        sub = fig.add_subplot(2, 5, i+1)
+        input_tensor = misclassified_images[i].unsqueeze(dim=0)
+        targets = [ClassifierOutputTarget(actual_targets[i])]
+        grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
+        grayscale_cam = grayscale_cam[0, :]
+        visualization = show_cam_on_image(unnormalize(misclassified_images[i].cpu()), grayscale_cam, use_rgb=True, image_weight=0.7)
+
+        # npimg = unnormalize(misclassified_images[i].cpu())
+        # plt.imshow(npimg, cmap='gray', interpolation='none')
+
+        # npimg = unnormalize(misclassified_images[i].cpu())
+        plt.imshow(visualization)
+        sub.set_title("Actual: {}, Pred: {}".format(actual_labels[i], predicted_labels[i]),color='red')
+    plt.tight_layout()
+    plt.show()
 
 def plot_grad_cam_images(model, test_loader, classes, device):
     # set model to evaluation mode
